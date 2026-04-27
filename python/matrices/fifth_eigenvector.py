30aaf4d58530eabe65ef65a2ed80db2c28be0d67
Austin <jakes001@rangers.uwp.edu>
2026-04-27 19:46:02 +0000

    matrix

--- python/matrices/fifth_eigenvector.py
+++ python/matrices/fifth_eigenvector.py
@@ -1,1 +1,8 @@
+import numpy as np
 
+
+main_diag = 2 * np.ones(5)
+off_diag = -1 * np.ones(4)
+
+matrix = np.diag(main_diag, k=0) + np.diag(off_diag, k=1) + np.diag(off_diag, k=-1)
+print(f'{matrix}')
--- python/matrices/tenth_eigenvector.py
+++ python/matrices/tenth_eigenvector.py
@@ -1,1 +1,8 @@
+import numpy as np
 
+
+main_diagonal = 2 * np.ones(10)
+off_diagonal = -1 * np.ones(9)
+
+matrix = np.diag(main_diagonal, k=0) + np.diag(off_diagonal, k=1) + np.diag(off_diagonal, k=-1)
+print(f'{matrix}')
